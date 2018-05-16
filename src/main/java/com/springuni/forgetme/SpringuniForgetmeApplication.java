7c326499d104b2db4f787be40c91aff1d455898b
Laszlo Csontos <github@laszlocsontos.com>
2018-05-16 11:35:21 +0200

    SUFM-1 Committing skeleton project generated by start.spring.io

---
+++ src/main/java/com/springuni/forgetme/SpringuniForgetmeApplication.java
@@ -0,0 +1,12 @@
+package com.springuni.forgetme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringuniForgetmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringuniForgetmeApplication.class, args);
+	}
+}
