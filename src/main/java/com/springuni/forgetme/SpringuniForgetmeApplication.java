174cbc3040588a236a7a1af76c3733d7a427f69f
Laszlo Csontos <github@laszlocsontos.com>
2018-05-16 14:06:50 +0200

    SUFM-1 Fix Checkstyle violations

--- src/main/java/com/springuni/forgetme/SpringuniForgetmeApplication.java
+++ src/main/java/com/springuni/forgetme/SpringuniForgetmeApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringuniForgetmeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringuniForgetmeApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringuniForgetmeApplication.class, args);
+  }
+
 }
