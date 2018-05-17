ee6c8a22b52c4d1633f6ccbe85c3daee4eecd6ad
Laszlo Csontos <github@laszlocsontos.com>
2018-05-17 15:00:04 +0200

    SUFM-4 Add WebhookOutboundChannelConfig

---
+++ src/main/java/com/springuni/forgetme/datahandler/WebhookOutboundChannelConfig.java
@@ -0,0 +1,31 @@
+package com.springuni.forgetme.datahandler;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.amqp.dsl.Amqp;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Transformers;
+import org.springframework.messaging.MessageChannel;
+
+@Configuration
+public class WebhookOutboundChannelConfig {
+
+  @Bean
+  public MessageChannel webhookOutboundChannel() {
+    return new DirectChannel();
+  }
+
+  @Bean
+  public IntegrationFlow webhookOutboundFlow(
+      MessageChannel webhookOutboundChannel, AmqpTemplate amqpTemplate) {
+
+    return IntegrationFlows.from(webhookOutboundChannel)
+        .transform(Transformers.toJson())
+        .handle(Amqp.outboundAdapter(amqpTemplate))
+        .get();
+  }
+
+}
