13dd016c025cb86e2913c48e60832cd4bf0c78ff
Laszlo Csontos <github@laszlocsontos.com>
2018-05-22 15:39:06 +0200

    SUFM-8 Fix SpringuniForgetmeApplicationTests

--- forgetme-integration-tests/src/test/java/com/springuni/forgetme/SpringuniForgetmeApplicationTests.java
+++ forgetme-integration-tests/src/test/java/com/springuni/forgetme/SpringuniForgetmeApplicationTests.java
@@ -2,11 +2,13 @@ package com.springuni.forgetme;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@SpringBootApplication
 public class SpringuniForgetmeApplicationTests {
 
   @Test
