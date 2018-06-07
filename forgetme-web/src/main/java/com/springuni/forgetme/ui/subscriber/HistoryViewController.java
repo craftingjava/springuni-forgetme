0ea98e37af9fac56c5e8d56bfa042a13408bf266
Laszlo Csontos <github@laszlocsontos.com>
2018-06-07 18:37:15 +0200

    SUFM-16 Implement subscription history page

--- forgetme-web/src/main/java/com/springuni/forgetme/ui/subscriber/HistoryViewController.java
+++ forgetme-web/src/main/java/com/springuni/forgetme/ui/subscriber/HistoryViewController.java
@@ -1,7 +1,17 @@
 package com.springuni.forgetme.ui.subscriber;
 
+import static java.util.stream.Collectors.toList;
+
+import com.springuni.forgetme.core.model.SubscriptionStatus;
+import com.springuni.forgetme.subscriber.model.Subscriber;
+import com.springuni.forgetme.subscriber.model.Subscription;
 import com.springuni.forgetme.subscriber.service.SubscriberService;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +23,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequiredArgsConstructor
 public class HistoryViewController extends AbstractViewController {
 
-  static final String MODEL_NAME = "subscriber";
+  static final String MODEL_NAME = "subscriptions";
   static final String VIEW_NAME = "pages/history";
 
   private final SubscriberService subscriberService;
@@ -30,6 +40,30 @@ public class HistoryViewController extends AbstractViewController {
 
   @Override
   protected void populateModel(String email, ModelAndView modelAndView) {
+    subscriberService.findSubscriber(email)
+        .map(Subscriber::getSubscriptions)
+        .map(subscriptions -> subscriptions.stream()
+            .map(this::toHistoryEntries)
+            .flatMap(Collection::stream)
+            .collect(toList())
+        )
+        .ifPresent(it -> modelAndView.addObject(MODEL_NAME, it));
+  }
+
+  private List<HistoryEntry> toHistoryEntries(Subscription subscription) {
+    UUID dataHandlerId = subscription.getDataHandlerId();
+    return subscription.getSubscriptionChanges()
+        .stream()
+        .map(it -> new HistoryEntry(dataHandlerId, it.getStatus(), it.getEventTimestamp()))
+        .collect(toList());
+  }
+
+  @Value
+  public static class HistoryEntry {
+
+    private UUID dataHandlerId;
+    private SubscriptionStatus status;
+    private LocalDateTime eventTimestamp;
 
   }
 
